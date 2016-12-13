3c5b182f77ee321fb29d4eaf6831a81f53f60ab6
Rifaut Alexis <Rifaut Alexis@DESKTOP-VB28T7L.home>
2016-12-14 00:04:05 +0100

    Vol: 	- Suppression d'ancien code dans nbCroisment 	- Ajout des IllegalArgumentException

--- src/Vol.java
+++ src/Vol.java
@@ -1,3 +1,4 @@
+
 /**
  * Projet algo
  *
@@ -24,6 +25,15 @@ public class Vol {
      * passé
      */
     public Vol(Date date, String pilote, Coordonnees[] tableCoordonnees) {
+        if (date == null) {
+            throw new IllegalArgumentException("date est null, ne peut-être null !");
+        }
+        if (pilote == null) {
+            throw new IllegalArgumentException("pilote est null, ne peut-être null !");
+        }
+        if (tableCoordonnees == null) {
+            throw new IllegalArgumentException("tableCoordonnees est null, ne peut-être null !");
+        }
         this.date = date;
         this.pilote = pilote;
         this.tableCoordonnees = tableCoordonnees;
@@ -148,6 +158,10 @@ public class Vol {
      * @return Coodonnees la plus proche de la coordonnée cible
      */
     public Coordonnees distancePlusProcheCible(Coordonnees cible) {
+        if (cible == null) {
+            throw new IllegalArgumentException("cible est null, ne peut-être null !");
+        }
+
         double distanceMin = cible.distance(tableCoordonnees[0]);
         Coordonnees coordonneesMin = tableCoordonnees[0];
         for (int i = 1; i < NB_COORDONNEE; i++) {
@@ -181,16 +195,20 @@ public class Vol {
      * contournements
      */
     public double distanceContournement(int nbContournement) {
+        if (nbContournement < 0 || nbContournement > 2) {
+            throw new IllegalArgumentException("nbContournement n'est pas compris entre 0 et 2 inclus !");
+        }
+
         double distance = tableCoordonnees[0].distance(tableCoordonnees[1]) + tableCoordonnees[NB_COORDONNEE - 1].distance(tableCoordonnees[1]);
-        
-        switch(nbContournement){
-            case 0 :
+
+        switch (nbContournement) {
+            case 0:
                 distance = contournement0();
                 break;
-            case 1 :
+            case 1:
                 distance = contournement1(distance);
                 break;
-            case 2 :
+            case 2:
                 distance = contournement2(distance);
                 break;
         }
@@ -202,6 +220,10 @@ public class Vol {
     }
 
     private double contournement1(double distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException("distance ne peut pas être négatif !");
+        }
+        
         for (int i = 0; i < NB_COORDONNEE; i++) {
             if (distance < tableCoordonnees[0].distance(tableCoordonnees[i]) + tableCoordonnees[NB_COORDONNEE - 1].distance(tableCoordonnees[i])) {
                 distance = tableCoordonnees[0].distance(tableCoordonnees[i]) + tableCoordonnees[NB_COORDONNEE - 1].distance(tableCoordonnees[i]);
@@ -210,7 +232,11 @@ public class Vol {
         return distance;
     }
 
-    private double contournement2(double distance) {        
+    private double contournement2(double distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException("distance ne peut pas être négatif !");
+        }
+        
         for (int i = 0; i < NB_COORDONNEE; i++) {
             for (int j = 0; j < NB_COORDONNEE; j++) {
                 if (distance < tableCoordonnees[0].distance(tableCoordonnees[i]) + tableCoordonnees[i].distance(tableCoordonnees[j]) + tableCoordonnees[NB_COORDONNEE - 1].distance(tableCoordonnees[j])) {
@@ -230,47 +256,17 @@ public class Vol {
         int cpt = 0;
 
         for (int i = 0; i < NB_COORDONNEE; i++) {
-            for(int j = i + 2; j < NB_COORDONNEE - 1; j ++){
-                if(Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i + 1], tableCoordonnees[j], tableCoordonnees[j + 1])){
-                    cpt ++;
-                    if(Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i + 1], tableCoordonnees[j], tableCoordonnees[j])){
-                        cpt --;
-                    } else if(Coordonnees.segmentsCroises(tableCoordonnees[i + 1], tableCoordonnees[i + 1], tableCoordonnees[j], tableCoordonnees[j + 1])){
-                        cpt --;
+            for (int j = i + 2; j < NB_COORDONNEE - 1; j++) {
+                if (Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i + 1], tableCoordonnees[j], tableCoordonnees[j + 1])) {
+                    cpt++;
+                    if (Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i + 1], tableCoordonnees[j], tableCoordonnees[j])) {
+                        cpt--;
+                    } else if (Coordonnees.segmentsCroises(tableCoordonnees[i + 1], tableCoordonnees[i + 1], tableCoordonnees[j], tableCoordonnees[j + 1])) {
+                        cpt--;
                     }
                 }
             }
         }
-        
-        
-        
-//        if (tableCoordonnees[0].equals(tableCoordonnees[NB_COORDONNEE - 1])) {
-//            cpt++;
-//        }
-//        for (int i = 0; i < NB_COORDONNEE; i++) {
-//            for (int j = i + 1; j < NB_COORDONNEE; j++) {
-//                if (tableCoordonnees[j].equals(tableCoordonnees[i])) {
-//                    cpt++;
-//                }
-//                if (j + 1 != NB_COORDONNEE && i + 1 != NB_COORDONNEE && Coordonnees.segmentsCroises(tableCoordonnees[j], tableCoordonnees[j + 1], tableCoordonnees[i], tableCoordonnees[i + 1])) {
-//                    cpt++;
-//                }
-//            }
-//        }
-
-
-//        for (int i = 1; i < NB_COORDONNEE; i++) {
-//            for(int j = 0; j < i; j++) {
-//                if(tableCoordonnees[i].equals(tableCoordonnees[j]))
-//                    cpt++;
-//                if(j > 0 && tableCoordonnees[i] != tableCoordonnees[j] && tableCoordonnees[i] != tableCoordonnees[j-1] && tableCoordonnees[i-1] != tableCoordonnees[j] && tableCoordonnees[i-1] != tableCoordonnees[j-1])
-//                if(j > 0 && j + 1 < i && Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i-1], tableCoordonnees[j], tableCoordonnees[j-1]))
-//                    cpt++;
-//            }
-//        }
-//        if(tableCoordonnees[0].equals(tableCoordonnees[NB_COORDONNEE-1])){
-//            cpt ++;
-//        }
         return cpt;
     }
 
@@ -281,13 +277,17 @@ public class Vol {
      * @return Le nombre de cibles atteintes
      */
     public int nbCiblesAtteintes(Coordonnees[] cibles) {
+        if (cibles  == null) {
+            throw new IllegalArgumentException("cibles est nul, ne peu-être null ! !");
+        }
+        
         int cpt = 0;
         for (Coordonnees cible : cibles) {
-            for (int i = 0; i < NB_COORDONNEE; i ++) {
+            for (int i = 0; i < NB_COORDONNEE; i++) {
                 if (tableCoordonnees[i].equals(cible)) {
                     cpt++;
                     break;
-                }else if(i  <= NB_COORDONNEE - 1 && Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i + 1], cible, cible)){
+                } else if (i <= NB_COORDONNEE - 1 && Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i + 1], cible, cible)) {
                     cpt++;
                     break;
                 }
@@ -297,20 +297,24 @@ public class Vol {
     }
 
     public int nbCibleAtteintesLorsParcours(Coordonnees[] ParcoursImpose) {
+        if (ParcoursImpose  == null) {
+            throw new IllegalArgumentException("ParcoursImpose est nul, ne peu-être null ! !");
+        }
+        
         int cpt = 0;
         int cpt2 = 0;
         for (Coordonnees coordonnees : ParcoursImpose) {
-            for (int i = 0; i < NB_COORDONNEE; i ++) {
-                if(tableCoordonnees[i].equals(coordonnees)) {
+            for (int i = 0; i < NB_COORDONNEE; i++) {
+                if (tableCoordonnees[i].equals(coordonnees)) {
                     cpt++;
                     break;
-                }else if(i  <= NB_COORDONNEE - 1 && Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i + 1], coordonnees, coordonnees)){
+                } else if (i <= NB_COORDONNEE - 1 && Coordonnees.segmentsCroises(tableCoordonnees[i], tableCoordonnees[i + 1], coordonnees, coordonnees)) {
                     cpt++;
                     break;
                 }
             }
             cpt2++;
-            if(cpt != cpt2){
+            if (cpt != cpt2) {
                 return cpt;
             }
         }
